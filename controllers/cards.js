a49c8595cd2b9999ddac32d085a5ddf06f5a7952
Andrey Mirenkov <Andreymirenkov@yandex.ru>
2022-06-15 17:28:04 +0300

    Проектная работа 13

--- controllers/cards.js
+++ controllers/cards.js
@@ -6,11 +6,69 @@ module.exports.getCard = (req, res) => {
 };
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
-  console.log(req.user._id);
   Card.create({ name, link, owner: req.user._id }).then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные в методы создания карточки' });
+      } return res.status(500).send({ message: err.message });
+    });
 };
 module.exports.deleteCard = (req, res) => {
-  Card.findByIdAndRemove(req.params.cardId).then(() => res.status(200).send({ message: 'Картинка удалена' }))
-    .catch(() => res.status(500).send({ message: 'Ошибка на сервере' }));
+  Card.findByIdAndRemove(req.params.cardId).then((card) => {
+    if (card === null) {
+      const error = new Error('Запрашиваемая карточка не найдена');
+      error.name = 'NotFoundError';
+      throw error;
+    }
+    res.status(200).send({ message: 'Картинка удалена' });
+  })
+    .catch((err) => {
+      if (err.name === 'NotFoundError') {
+        return res.status(404).send({ message: err.message });
+      } if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Неправильный формат данных ID карточки' });
+      } return res.status(500).send({ message: 'Ошибка на сервере' });
+    });
+};
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  ).then((card) => {
+    if (card === null) {
+      const error = new Error('Запрашиваемая карточка не найдена');
+      error.name = 'NotFoundError';
+      throw error;
+    }
+    res.status(201).send({ message: card });
+  })
+    .catch((err) => {
+      if (err.name === 'NotFoundError') {
+        return res.status(404).send({ message: err.message });
+      } if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Неправильный формат данных ID карточки' });
+      } return res.status(500).send({ message: err.name });
+    });
+};
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  ).then((card) => {
+    if (card === null) {
+      const error = new Error('Запрашиваемая карточка не найдена');
+      error.name = 'NotFoundError';
+      throw error;
+    }
+    res.status(201).send({ message: card });
+  })
+    .catch((err) => {
+      if (err.name === 'NotFoundError') {
+        return res.status(404).send({ message: err.message });
+      } if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Неправильный формат данных ID карточки' });
+      } return res.status(500).send({ message: 'Ошибка на сервере' });
+    });
 };
--- controllers/users.js
+++ controllers/users.js
@@ -2,26 +2,73 @@ const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
   User.find({}).then((users) => res.send({ data: users }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch(() => res.status(500).send({ message: 'Ошибка на сервере' }));
 };
 module.exports.getUser = (req, res) => {
-  User.findById(req.params.userid).then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+  User.findById(req.params.userid).then((user) => {
+    if (user === null) {
+      const error = new Error('Запрашиваемый пользователь не найден');
+      error.name = 'NotFoundError';
+      throw error;
+    }
+    res.send({ data: user });
+  })
+    .catch((err) => {
+      if (err.name === 'NotFoundError') {
+        return res.status(404).send({ message: err.message });
+      } if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Неправильный формат данных ID пользователя' });
+      } return res.status(500).send({ message: 'Ошибка на сервере' });
+    });
 };
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar }).then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные в методы создания пользователя' });
+      } return res.status(500).send({ message: 'Ошибка на сервере' });
+    });
 };
 module.exports.patchProfile = (req, res) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((user) => {
+      if (user === null) {
+        const error = new Error('Запрашиваемый пользователь не найден');
+        error.name = 'NotFoundError';
+        throw error;
+      }
+      res.send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === 'NotFoundError') {
+        return res.status(404).send({ message: err.message });
+      } if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные в методы обновления пользователя' });
+      } if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Неправильный формат данных ID пользователя' });
+      } return res.status(500).send({ message: 'Ошибка на сервере' });
+    });
 };
 module.exports.patchAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((user) => {
+      if (user === null) {
+        const error = new Error('Запрашиваемый пользователь не найден');
+        error.name = 'NotFoundError';
+        throw error;
+      }
+      res.send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === 'NotFoundError') {
+        return res.status(404).send({ message: err.message });
+      } if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные в методы обновления аватара' });
+      } if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Неправильный формат данных ID пользователя' });
+      } return res.status(500).send({ message: 'Ошибка на сервере' });
+    });
 };
